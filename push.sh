9fc4fa921188f62562fcde8cf436dd189403e3dd
Jeromine SAUVEUR <jsauveur@e1r1p5.42.fr>
2022-01-20 18:09:02 +0100

    test

---
+++ push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git add *
+git commit -m "$1"
+git push
+
+#echo "# sdfghjkl" >> README.md
+#git init
+#git add README.md
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin git@github.com:eNimorej/sdfghjkl.git
+#git push -u origin main
